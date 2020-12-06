8051745b623677882baf97216c330513cd3810e0
Mykola Gutsaliuk <ozonometer@gmail.com>
2020-12-06 17:36:08 -0600

    Moved lcd method to its own class.

---
+++ src/lcd_lib.h
@@ -0,0 +1,18 @@
+//
+// Created by Mykola on 12/6/2020.
+//
+
+#ifndef ARDUINO_MEGA_LCD_LCD_LIB_H
+#define ARDUINO_MEGA_LCD_LCD_LIB_H
+
+
+class lcd_lib {
+public:
+    lcd_lib();
+    void Lcd_Init();
+    void LCD_Clear(unsigned int);
+
+};
+
+
+#endif //ARDUINO_MEGA_LCD_LCD_LIB_H
